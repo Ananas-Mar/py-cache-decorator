a40f6a1420a97b3c22731f7031a81da495cf1f10
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-05-10 17:38:57 +0200

    first-solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    check_dict = {}
+    def wrapper(*args, **kwargs) -> Any:
+        if args in check_dict or kwargs.values() in check_dict:
+            print("Getting from cache")
+            return check_dict[args]
+        elif args not in check_dict:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            check_dict[args] = result
+            return result
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            check_dict[kwargs.values()] = result
+            return result
+    return wrapper
